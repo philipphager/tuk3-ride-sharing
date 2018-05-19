a0f19ac308e0c924783fea0fa2693ba2ef36f789
Victor Künstler <victor.kuenstler@outlook.com>
2018-05-19 16:41:08 +0200

    Add Play Button

--- frontend/app/container/Menu.js
+++ frontend/app/container/Menu.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Row, Col, Slider, Spin } from 'antd';
+import { Row, Col, Slider, Spin, Button } from 'antd';
 
 import * as actions from '../actions/trajectoryActions';
 import TrajectorySelect from '../components/TrajectorySelect';
@@ -12,24 +12,56 @@ class Menu extends Component {
     getTrajectory: PropTypes.func.isRequired,
     changeMaxTrajectoryFrame: PropTypes.func.isRequired,
     trajectoryIds: PropTypes.arrayOf(PropTypes.number),
+    maxFrame: PropTypes.number,
   }
 
   static defaultProps = {
     trajectoryIds: null,
+    maxFrame: 0,
   }
 
-  componentWillMount = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlaying: false,
+    };
+  }
+
+  componentWillMount() {
     this.props.getTrajectories();
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => {
+        if (this.state.isPlaying) {
+          this.playingTick();
+        }
+      },
+      0,
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   handleTimeChange = (newValue) => {
     this.props.changeMaxTrajectoryFrame(newValue);
   }
 
+  playingTick = () => {
+    this.props.changeMaxTrajectoryFrame(this.props.maxFrame + 100);
+  }
+
   handleFormat = (value) => {
     const hour = (`0${Math.floor(value / 120)}`).slice(-2);
     const minute = (`0${Math.floor((value / 2) - (60 * hour))}`).slice(-2);
     return `${hour}:${minute}`;
+  }
+
+  handlePlay = () => {
+    this.setState({ isPlaying: !this.state.isPlaying });
   }
 
   render() {
@@ -46,10 +78,17 @@ class Menu extends Component {
             <Spin />
           )}
         </Col>
-        <Col span={16}>
+        <Col span={2}>
+          <Button
+            type="primary"
+            icon={this.state.isPlaying ? 'pause' : 'caret-right'}
+            onClick={this.handlePlay}
+          />
+        </Col>
+        <Col span={14}>
           <Slider
             min={0}
-            defaultValue={2880}
+            value={this.props.maxFrame}
             max={2880}
             onChange={this.handleTimeChange}
             tipFormatter={this.handleFormat}
@@ -63,6 +102,7 @@ class Menu extends Component {
 function mapStateToProps({ trajectories }) {
   return {
     trajectoryIds: trajectories.trejectoryIds,
+    maxFrame: trajectories.maxFrame,
   };
 }
 
