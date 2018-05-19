153a44b703957c92ec049a61e46e8f79410465a5
Victor Künstler <victor.kuenstler@outlook.com>
2018-05-19 15:44:58 +0200

    Rewrite App Container as pure function
    It could also be moved into ./components folder... Maybe later

--- frontend/app/container/App.js
+++ frontend/app/container/App.js
@@ -1,24 +1,22 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import 'antd/dist/antd.css';
 import { Row } from 'antd';
 import ResizableMap from './Map';
 import Menu from './Menu';
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Row>
-          <div style={{ width: '100vw', height: '80vh' }}>
-            <ResizableMap />
-          </div>
-        </Row>
-        <Row>
-          <Menu />
-        </Row>
-      </Fragment>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Fragment>
+      <Row>
+        <div style={{ width: '100vw', height: '80vh' }}>
+          <ResizableMap />
+        </div>
+      </Row>
+      <Row>
+        <Menu />
+      </Row>
+    </Fragment>
+  );
+};
 
 export default App;
