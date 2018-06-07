857193b195620bc91a4c2461a0d79be4957e2543
Victor Künstler <victor.kuenstler@outlook.com>
2018-06-07 17:18:50 +0200

    Move trajectory endpoint to frame-trajectory endpoint

--- backend/app/frame_trajectory/service.py
+++ backend/app/frame_trajectory/service.py
@@ -2,7 +2,7 @@ import pandas as pd
 from app.database.hana_connector import HanaConnection
 from app.geojson.frame_converter import frame_to_geojson,\
     trajectory_ids_to_json, frame_to_point
-from app.trajectory.sql import get_trajectory_by_id_sql,\
+from app.frame_trajectory.sql import get_trajectory_by_id_sql,\
     get_all_trajectory_ids_sql
 
 
