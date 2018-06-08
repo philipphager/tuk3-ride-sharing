55c37cf757b4ff58276246ab3d6a80ccd2b86ffe
Victor Künstler <victor.kuenstler@outlook.com>
2018-06-08 20:18:28 +0200

    Remove old frontend and add create-react-app typescript

--- frontend/app/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import mapReducer from './mapReducer';
-import trajectoriesReducer from './trajectoriesReducer';
-
-export default combineReducers({
-  map: mapReducer,
-  trajectories: trajectoriesReducer,
-});
