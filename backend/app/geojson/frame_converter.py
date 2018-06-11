ae6d73219f3656cade87c993d5fa7ddb36e76ff5
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-06-11 17:56:02 +0200

    Simplify nclob unpacking

--- backend/app/geojson/frame_converter.py
+++ backend/app/geojson/frame_converter.py
@@ -1,5 +1,6 @@
-from geojson import Feature, LineString
 import json
+
+from geojson import Feature, LineString
 
 
 def frame_to_geojson(frames):
@@ -20,18 +21,6 @@ def frame_to_point(data):
 
 
 def unpack_key_value_object(trajectory):
-    trajectory[1].read()
-    samples = json.loads(str(trajectory[1]))
-    array = []
-    for sample in samples:
-        array.append((sample[1], sample[2]))
-    return array
-
-
-# def get_sample(sample):
-#     print(sample)
-#     lat = sample[1]
-#     lon = sample[2]
-#     date_time = datetime.strptime(sample[0], '%Y-%m-%d %H:%M:%S')
-#     timestamp = date_time.hour * 60 * 60 + date_time.minute * 60 + date_time.second
-#     return (lat, lon, timestamp)
+    nclob = trajectory[1].read()
+    samples = json.loads(nclob)
+    return [(sample[1], sample[2]) for sample in samples]
