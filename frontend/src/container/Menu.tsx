700248da3d43e815a65c6ac717ecdc5c4e0b9b67
Victor Künstler <victor.kuenstler@outlook.com>
2018-06-14 09:20:56 +0200

    Add time slider logic

--- frontend/src/container/Menu.tsx
+++ frontend/src/container/Menu.tsx
@@ -16,11 +16,12 @@ interface Props {
 }
 
 interface State {
-    isPlaying: boolean,
-    stepSize: number,
-    time: Moment,
-    trajectoryIds: number[],
-    selectedTrajectories: number[],
+    isPlaying: boolean;
+    stepSize: number;
+    trajectoryTime: Moment;
+    sliderTime: number;
+    trajectoryIds: number[];
+    selectedTrajectories: number[];
 }
 
 class Menu extends React.Component<Props, State> {
@@ -29,7 +30,8 @@ class Menu extends React.Component<Props, State> {
         this.state = {
             isPlaying: false,
             stepSize: 1,
-            time: moment(),
+            trajectoryTime: moment(),
+            sliderTime: 43250,
             trajectoryIds: [],
             selectedTrajectories: []
         }
@@ -75,7 +77,7 @@ class Menu extends React.Component<Props, State> {
                     </Select>
                 </Col>
                 <Col span={4}>
-                    <TimePicker onChange={this.onTimeChange} value={this.state.time} />
+                    <TimePicker onChange={this.onTimeChange} value={this.state.trajectoryTime} />
                 </Col>
                 <Col span={4}>
                     <Select size="default" style={{ width: '100px'}} placeholder="Select Trajectory Id" onSelect={this.onTrajectorySelect}>
@@ -91,7 +93,10 @@ class Menu extends React.Component<Props, State> {
                             min={1}
                             max={86400}
                             marks={marks}
+                            value={this.state.sliderTime}
                             tipFormatter={this.handleFormat}
+                            onChange={this.handleSliderTimeChange}
+                            onAfterChange={this.handleSliderTimeChangeStop}
                         />
                     </Row>
                 </Col>
@@ -99,13 +104,14 @@ class Menu extends React.Component<Props, State> {
         );
     }
 
+
     private onTimeChange = (value: Moment) => {
         this.setState({
-            time: value,
+            trajectoryTime: value,
         })
     }
 
-    private onTrajectorySelect = (value: number) => {
+    private onTrajectorySelect = (value: number): void => {
         if(this.state.selectedTrajectories.indexOf(value) === -1) {
             this.setState({
                 selectedTrajectories: [... this.state.selectedTrajectories, value]
@@ -113,7 +119,7 @@ class Menu extends React.Component<Props, State> {
         }
     }
 
-    private onDatFormChange = (value: DataFormats) => {
+    private onDatFormChange = (value: DataFormats): void => {
         this.props.onDataFrameChange(value);
         axios.get(`/${value}/?limit=1000&offset=0`)
             .then(response => {
@@ -121,6 +127,16 @@ class Menu extends React.Component<Props, State> {
                     trajectoryIds: response.data.data.trip_ids
                 })
             })
+    }
+
+    private handleSliderTimeChange = (value: number): void => {
+        this.setState({
+            sliderTime: value,
+        });
+    }
+
+    private handleSliderTimeChangeStop = (value: number): void => {
+        console.log("Es ist passiert")
     }
 
     private handleFormat = (value: number): string => {
