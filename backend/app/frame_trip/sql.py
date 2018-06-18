b0eb5a6dfd4576a214638b764cc7ca796c9898c6
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-06-18 15:32:06 +0200

    Update frame trips endpoint

--- backend/app/frame_trip/sql.py
+++ backend/app/frame_trip/sql.py
@@ -1,27 +1,17 @@
 from app.database.const import FRAME_TRIPS_TABLE
 
 
-def get_all_trajectory_ids_sql():
-    return f'''
-      SELECT DISTINCT TRAJECTORY_ID
-      FROM {FRAME_TRIPS_TABLE}
-    '''
-
-
 def get_all_trip_ids_sql(offset, limit):
     return f'''
-      SELECT DISTINCT CONCAT(TRAJECTORY_ID, TRIP_ID)
+      SELECT DISTINCT ID
       FROM {FRAME_TRIPS_TABLE}
+      ORDER BY ID
       LIMIT {limit}
       OFFSET {offset}
     '''
 
 
 def get_trip_by_id_sql(trip_id, max_group_id):
-    # TODO: Remove!
-    trajectory_id = trip_id[:5]
-    trip_id = trip_id[5:]
-
     sql = 'SELECT GROUP_ID, IX, IY,'
     for i in range(1, 30):
         sql += f'''
@@ -30,8 +20,7 @@ def get_trip_by_id_sql(trip_id, max_group_id):
         sql += ',' if i < 29 else ''
     sql += f'''
     FROM {FRAME_TRIPS_TABLE} 
-    WHERE TRAJECTORY_ID = {trajectory_id}
-    AND TRIP_ID = {trip_id}
+    WHERE ID = {trip_id}
     AND GROUP_ID <= {max_group_id}
     ORDER BY GROUP_ID
     '''
