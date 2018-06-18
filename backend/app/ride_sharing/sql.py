947c26ca95c68e040ba64386f5aaaa621f7e0840
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-06-18 22:39:42 +0200

    Formatting

--- backend/app/ride_sharing/sql.py
+++ backend/app/ride_sharing/sql.py
@@ -11,31 +11,40 @@ def get_shared_rides_sql():
     threshold = 0.03
 
     sql = f'''
-          SELECT key_value.TRIP_ID, key_value.OBJ, frame.DISTANCE_START, frame.DISTANCE_END from
-          (
-              SELECT ID as TRIP_ID, OBJ from KEY_VALUE_TRIPS
-          ) key_value
-          inner join
-          (
-              SELECT TRIP_ID, DISTANCE_START, DISTANCE_END
-                  FROM 
-                      (SELECT s.ID as TRIP_ID, 
-                          SQRT(POWER({start_x} - s.IX + s.P{start_frame}X, 2) + POWER({start_y} - s.IY + s.P{start_frame}Y, 2)) as DISTANCE_START,
-                          SQRT(POWER({end_x} - e.IX + e.P{end_frame}X, 2) + POWER({end_y} - e.IY + e.P{end_frame}Y, 2)) as DISTANCE_END
-                          from FRAME_TRIPS s
-                          inner join FRAME_TRIPS e
-                              on s.GROUP_ID = {start_group_id}
-                              and s.P{start_frame}X IS NOT NULL 
-                              and s.P{start_frame}Y IS NOT NULL
-
-                              and e.GROUP_ID = {end_group_id}
-                              and e.P{end_frame}X IS NOT NULL 
-                              and e.P{end_frame}Y IS NOT NULL
-
-                              and s.ID = e.ID)
-                  WHERE DISTANCE_START <= {threshold} 
-                  AND DISTANCE_END <= {threshold}
-          ) frame
-          on key_value.TRIP_ID = frame.TRIP_ID
-          '''
+        SELECT 
+            key_value.TRIP_ID, 
+            key_value.OBJ,
+            frame.DISTANCE_START,
+            frame.DISTANCE_END
+        FROM (
+            SELECT 
+                ID as TRIP_ID, 
+                OBJ
+            FROM KEY_VALUE_TRIPS
+        ) key_value
+        INNER JOIN (
+            SELECT
+                TRIP_ID,
+                DISTANCE_START,
+                DISTANCE_END
+            FROM (
+                SELECT
+                    s.ID as TRIP_ID, 
+                    SQRT(POWER({start_x} - s.IX + s.P{start_frame}X, 2) + POWER({start_y} - s.IY + s.P{start_frame}Y, 2)) as DISTANCE_START,
+                    SQRT(POWER({end_x} - e.IX + e.P{end_frame}X, 2) + POWER({end_y} - e.IY + e.P{end_frame}Y, 2)) as DISTANCE_END
+                FROM FRAME_TRIPS s
+                INNER JOIN FRAME_TRIPS e
+                ON s.GROUP_ID = {start_group_id}
+                AND s.P{start_frame}X IS NOT NULL 
+                AND s.P{start_frame}Y IS NOT NULL
+                AND e.GROUP_ID = {end_group_id}
+                AND e.P{end_frame}X IS NOT NULL 
+                AND e.P{end_frame}Y IS NOT NULL
+                AND s.ID = e.ID
+            )
+            WHERE DISTANCE_START <= {threshold} 
+            AND DISTANCE_END <= {threshold}
+        ) frame
+        ON key_value.TRIP_ID = frame.TRIP_ID
+        '''
     return sql
