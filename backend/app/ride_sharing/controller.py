be94e6b9531e2556ad0c10276707ee0a38b753a8
Volker Schiewe <volker.schiewe@incapptic.com>
2018-06-25 17:27:22 +0200

    convert distance to threshold

--- backend/app/ride_sharing/controller.py
+++ backend/app/ride_sharing/controller.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 from app.ride_sharing.service import get_shared_rides
 
 ride_sharing_controller = Blueprint('shared_rides', __name__)
 
 
-@ride_sharing_controller.route('/<trip_id>/<threshold>')
-def get_shared_rides_for_trips(trip_id, threshold):
+@ride_sharing_controller.route('/<trip_id>/<distance>')
+def get_shared_rides_for_trips(trip_id, distance):
+    threshold = distance / 60 / 1852  # 1 Grad = 60 min, 1 min = 1852 m
     data = get_shared_rides(trip_id, threshold)
-    return jsonify(data)
+    return data
