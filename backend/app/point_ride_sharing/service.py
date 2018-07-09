7df35eab8defe2bc6a7446a448a1b615c70d5351
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-07-09 14:25:12 +0200

    Add timing

--- backend/app/point_ride_sharing/service.py
+++ backend/app/point_ride_sharing/service.py
@@ -1,9 +1,10 @@
 from app.database.hana_connector import HanaConnection
 from app.geojson.geojson_utils import create_geojson
-
+from app.utils import timer
 from app.point_ride_sharing.sql import get_shared_rides_sql
 
 
+@timer
 def get_shared_rides(trip_id, threshold):
     with HanaConnection() as connection:
         connection.execute(get_shared_rides_sql(trip_id, threshold))
