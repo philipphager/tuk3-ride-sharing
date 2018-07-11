9efb1cacd15776d3e466bee10d6b9c4219db6247
Victor Künstler <victor.kuenstler@outlook.com>
2018-07-11 10:26:27 +0200

    Fix conflicts

--- backend/app/point_ride_sharing/sql.py
+++ backend/app/point_ride_sharing/sql.py
@@ -1,4 +1,4 @@
-def get_shared_rides_sql(trip_id, threshold):
+def get_shared_rides_sql(trip_id, max_distance, max_time):
     return f'''
         SELECT *
         FROM TUK3_HNKS.Point_Trips
@@ -9,10 +9,10 @@ def get_shared_rides_sql(trip_id, threshold):
                 (SELECT timestamp, lon, lat FROM TUK3_HNKS.Point_Trips WHERE id = {trip_id} ORDER BY timestamp DESC LIMIT 1) E,
                 TUK3_HNKS.Point_Trips Trip_Start INNER JOIN TUK3_HNKS.Point_Trips Trip_End
                 ON Trip_Start.id = Trip_End.id
-            WHERE Trip_Start.timestamp = S.timestamp
-            AND Trip_End.timestamp = E.timestamp
-            AND SQRT(POWER(Trip_Start.lon -  S.lon, 2) + POWER(Trip_Start.lat -  S.lat, 2)) <= {threshold}
-            AND SQRT(POWER(Trip_End.lon -  E.lon, 2) + POWER(Trip_End.lat -  E.lat, 2)) <= {threshold}
+            WHERE Trip_Start.timestamp BETWEEN (S.timestamp - {max_time}) AND (S.timestamp + {max_time})
+            AND Trip_End.timestamp BETWEEN (E.timestamp - {max_time}) AND (E.timestamp + {max_time})
+            AND SQRT(POWER(Trip_Start.lon -  S.lon, 2) + POWER(Trip_Start.lat -  S.lat, 2)) <= {max_distance}
+            AND SQRT(POWER(Trip_End.lon -  E.lon, 2) + POWER(Trip_End.lat -  E.lat, 2)) <= {max_distance}
         )
         ORDER BY id, timestamp;
     '''
