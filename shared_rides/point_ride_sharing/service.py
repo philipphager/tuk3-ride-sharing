21812de3edcaec69dd37483b54735058819ed113
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-07-19 18:45:20 +0200

    Parallelism

--- shared_rides/point_ride_sharing/service.py
+++ shared_rides/point_ride_sharing/service.py
@@ -1,16 +1,18 @@
 import csv
 from os import remove
+import multiprocessing as mp
 
 from database.hana_connector import HanaConnection
 from point_ride_sharing.sql import get_shared_rides_sql, get_trip_ids
 
 
 class RideSharing:
-    def __init__(self, output, distance, time):
+    def __init__(self, output, distance, time, threads=8):
         self.output = output
         self.distance = distance
         self.time = time
         self.trip_to_shared_rides = dict()
+        self.threads = threads
         try:
             remove(self.output)
         except FileNotFoundError:
@@ -25,17 +27,21 @@ class RideSharing:
             connection.execute(get_trip_ids())
             cursor = connection.fetchall()
 
-            for [trip_id] in cursor:
-                trips = self.get_shared_rides(connection, trip_id)
-                self.trip_to_shared_rides[trip_id] = trips
-                print('Trip id:', trip_id, 'Ride sharing candidates:', trips)
+            chunk_size = 10000
+            trip_chunks = [cursor[i: i + chunk_size] for i in range(0, len(cursor), chunk_size)]
 
-    def get_shared_rides(self, connection, trip_id):
-        connection.execute(get_shared_rides_sql(trip_id, self.distance, self.time))
-        return [trip for [trip] in connection.fetchall()]
+            pool = mp.Pool(processes=self.threads)
+            [pool.apply(self.get_shared_rides, args=(chunk,)) for chunk in trip_chunks]
+
+    def get_shared_rides(self, chunk):
+        with HanaConnection() as connection:
+            for [trip_id] in chunk:
+                print('Trip id:', trip_id)
+                connection.execute(get_shared_rides_sql(trip_id, self.distance, self.time))
+                self.trip_to_shared_rides[trip_id] = [trip for [trip] in connection.fetchall()]
 
     def save_to_file(self):
-        with open(self.output, mode='a', encoding='utf-8') as f:
+        with open(self.output, mode='w', encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerow(['trip_id', 'count', 'shared_ride_ids'])
             for trip_id, shared_rides in self.trip_to_shared_rides.items():
