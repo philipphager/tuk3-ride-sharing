2fc8b7ad606c71fa11b32b3fd5cb4ef92e8b1554
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-07-23 09:55:03 +0200

    Add default paths to rest tester

--- rest_tester/main.py
+++ rest_tester/main.py
@@ -9,8 +9,8 @@ def cli():
 
 
 @cli.command()
-@click.option('--input', help='Path to input')
-@click.option('--output', help='Path to output')
+@click.option('--input', default='data/test.yml', help='Path to input')
+@click.option('--output', default='data/results.csv', help='Path to output')
 def run(input, output):
     config = open_yaml(input)
     run_requests(config, output)
